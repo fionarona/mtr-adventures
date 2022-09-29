0ce82a0415df4dcc14fc9f8e3f1e1f9143aed286
FIONA <choco.nene@gmail.com>
2022-09-29 22:33:24 +0800

    okay

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mtr-adventures
+
+Benny has forgotten his lunch box at home and his mom needs YOUR help to bring it to him!
---
+++ scripts/main.js
@@ -0,0 +1,91 @@
+const $benny = $('.benny')
+const $sky = $('.sky')
+const $window = $('.windows')
+const $gameBoard = $('.grid')
+const $squares = $('.squares')
+const $mole = $('.mole')
+const $sally = $('.sally')
+const $intro = $('.intro')
+const $gameOver = $('.gameOver')
+const $title =$('.title')
+
+const $score = $('#score')
+let result = 0
+let timerId = null
+let hitPosition, sallyPosition, bentlyPosition
+
+const randomSquare = () => {
+  $squares.removeClass('mole')
+  let randomSquare = $squares.eq(Math.floor(Math.random()*30))
+  randomSquare.addClass('mole')
+
+  hitPosition = randomSquare.attr('id')
+  }
+
+const randomSally = () => {
+  $squares.removeClass('sally')
+  let randomSquare = $squares.eq(Math.floor(Math.random()*30))
+  randomSquare.addClass('sally')
+
+  sallyPosition = randomSquare.attr('id')
+  }
+
+const randomBently = () => {
+  $squares.removeClass('bently')
+  let randomSquare = $squares.eq(Math.floor(Math.random()*30))
+  randomSquare.addClass('bently')
+
+  bentlyPosition = randomSquare.attr('id')
+}
+
+moveMole = () => {
+  timerId = setInterval(randomSquare, 1000)
+}
+moveSally = () => {
+  timerId = setInterval(randomSally, 1000)
+}
+moveBently = () => {
+  timerId = setInterval(randomBently, 500)
+}
+
+$benny.on('click', function(e) {
+  $intro.remove()
+  $benny.hide()
+  moveMole()
+})
+
+
+$squares.on('click', function(e) {
+  if ($(e.currentTarget).attr('id') == hitPosition) {
+    result ++
+    $score.text(result)
+    hitPosition = null}
+    else clearInterval(timerId)
+  if (result >= 5){
+    moveSally()}
+  if (result >= 10){
+    moveBently()
+  }})
+
+$squares.on('click', function(e) {
+  if ($(e.currentTarget).attr('id') == bentlyPosition) {
+    $gameOver.show()
+    $score.show()
+    clearInterval(timerId)
+    timerId = undefined
+  }
+})
+
+$squares.on('click', function(e) {
+  if ($(e.currentTarget).attr('id') == sallyPosition) {
+    $gameOver.show()
+    $score.show()
+    clearInterval(timerId)
+    timerId = undefined}
+})
+
+$benny.on('click', function(e) {
+  $intro.remove()
+  $benny.hide()
+  moveMole()
+})
